eb58c16834c62770ade927923e7a5db50b2ad3c4
Adriano Melo <adriano.melo@oviva.com>
2024-10-14 14:53:35 +0200

    add log statement

--- build/sample-event/js/customEvent.js
+++ build/sample-event/js/customEvent.js
@@ -107,6 +107,7 @@ define(["postmonger"], function (Postmonger) {
 
         payload.dataExtensionId = "E195C311-BFBF-47AF-985E-3703ABB154FB";
 
+        console.log({payload: JSON.stringify(payload, null, 2)});
         connection.trigger("updateEvent", payload);
     }
 });
